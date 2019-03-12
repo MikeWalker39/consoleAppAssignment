b7ad7a87f2701c768f4a38d36961c3c8dc5d6ee5
Mike Walker <mikewalker@Mikes-MacBook-Pro.local>
2019-03-12 10:15:31 -0500

    install mocha / run first test

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ app.get("/url", (req, res, next) => {
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+app.get('/', (req, res) => {
+    res.send("Hello World")
 
+});
+
+app.post('/records/gender', (req, res) => {
+  res.send("Post request to homepage")
+})
+
+app.post("/records", (req, res) => {
+    const body = req.body;
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
+
 });
+
+
